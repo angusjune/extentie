c12ea308a5884e1f4f2ff05596e9c55a794a08dc
angusjune <angus.is.online@gmail.com>
2022-06-14 12:37:34 +0800

    a11y

--- src/extList.js
+++ src/extList.js
@@ -188,7 +188,7 @@ export class ExtList extends LitElement {
                     <slot></slot>
                 </label>
                 <div class="list__actions">
-                    <a class="list__actions-item list__actions-item--delete" href="#" @click="${this._handleClickDelete}" tabindex="0" aria-label="uninstall ${this.title}">
+                    <a class="list__actions-item list__actions-item--delete" href="#" @click="${this._handleClickDelete}" tabindex="-1" aria-label="uninstall ${this.title}">
                         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.5 14.25C4.5 15.075 5.175 15.75 6 15.75H12C12.825 15.75 13.5 15.075 13.5 14.25V5.25H4.5V14.25ZM14.25 3H11.625L10.875 2.25H7.125L6.375 3H3.75V4.5H14.25V3Z" fill="black"/></svg>                    
                     </a>
                     ${this.isApp ? 
